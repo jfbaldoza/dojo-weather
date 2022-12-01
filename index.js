4667013f72422d3a44cb7c3e7ebf7f366588400c
Jan Francis Baldoza <you@example.com>
2022-12-01 06:40:10 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+// let cookie = document.querySelector('.cookie')
+// let acceptBtn = document.querySelector('.acceptBTN')
+
+// function city() {
+//     alert('Loading weather report')
+// }
+
+// function acceptBTN() {
+//     cookie.remove()
+// }
+
+console.log('hi')
+
+console.log('hi bye')
